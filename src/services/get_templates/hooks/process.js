988028dd64d2f196601031f8f3e8546201f45042
gugiapi <web2avi@gmail.com>
2016-11-16 13:27:31 +0200

    new endpoints

---
+++ src/services/get_templates/hooks/process.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// src/services/message/hooks/process.js
+//
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/hooks/readme.html
+
+module.exports = function(options) {
+  return function(hook) {
+
+    // Override the original data
+    hook.data = {
+      // Set the user id
+      results: "No template for bot"
+    };
+  };
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,15 @@
 'use strict';
 const addBot = require('./add_bot');
+const getKeywords = require('./get_keywords');
+const addKeyword = require('./add_keyword');
+const connectKeyword = require('./connect_keyword');
+const getTemplates = require('./get_templates');
+const addTemplate = require('./add_template');
+const connectTemplate = require('./connect_template');
+const addPage = require('./add_page');
+const recommendBot = require('./recommend_bot');
+const processPage = require('./process_page');
+
 const message = require('./message');
 const authentication = require('./authentication');
 const user = require('./user');
@@ -12,4 +22,14 @@ module.exports = function() {
   app.configure(user);
   app.configure(message);
   app.configure(addBot);
+  app.configure(getKeywords);
+  app.configure(connectKeyword);
+  app.configure(addKeyword);
+  app.configure(getTemplates);
+  app.configure(addTemplate);
+  app.configure(connectTemplate);
+  app.configure(addPage);
+  app.configure(recommendBot);
+  app.configure(processPage);
+  
 };
